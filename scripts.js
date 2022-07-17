9ada6d99dcce048fba9f485c0af5b119b36bd65e
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-16 22:56:28 -0700

    added none option in index.html and js file.

--- scripts.js
+++ scripts.js
@@ -18,6 +18,7 @@ Pizza.prototype.assignId = function () {
 //Business Logic for picking toppings and size
 
 function Pick(firstP, secondP, thirdP, fourthP, fifthP, sizeP) {
+  this.none = none;
   this.firstP = firstP;
   this.secondP = secondP;
   this.thirdP = thirdP;
@@ -30,22 +31,32 @@ function Pick(firstP, secondP, thirdP, fourthP, fifthP, sizeP) {
 Pick.prototype.Count = function () {
   if (this.firstP === "pepperoni" || this.firstP === "sausage" || this.firstP === "green bell pepper" || this.firstP === "black olives" || this.firstP === "mushrooms" || this.firstP === "chicken") {
     this.total += 3;
+  } else {
+    this.total += 0;
   }
 
   if (this.secondP === "pepperoni" || this.secondP === "sausage" || this.secondP === "green bell pepper" || this.secondP === "black olives" || this.secondP === "mushrooms" || this.secondP === "chicken") {
     this.total += 3;
+  } else {
+    this.total += 0;
   }
 
   if (this.thirdP === "pepperoni" || this.thirdP === "sausage" || this.thirdP === "green bell pepper" || this.thirdP === "black olives" || this.thirdP === "mushrooms" || this.thirdP === "chicken") {
     this.total += 3;
+  } else {
+    this.total += 0;
   }
 
   if (this.fourthP === "pepperoni" || this.fourthP === "sausage" || this.fourthP === "green bell pepper" || this.fourthP === "black olives" || this.fourthP === "mushrooms" || this.fourthP === "chicken") {
     this.total += 3;
+  } else {
+    this.total += 0;
   }
 
   if (this.fifthP === "pepperoni" || this.fifthP === "sausage" || this.fifthP === "green bell pepper" || this.fifthP === "black olives" || this.fifthP === "mushrooms" || this.fifthP === "chicken") {
     this.total += 3;
+  } else {
+    this.total += 0;
   }
 };
 
