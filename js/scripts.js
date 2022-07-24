99d5d9c5c205794bf55d454ab4f73b16d11a8455
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-23 22:09:13 -0700

    UI shows size and change price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //Business Logic for picking toppings and size
 
 function Pizza(first, second, third, fourth, fifth, size) {
-  this.firstP = first;
-  this.secondP = second;
-  this.thirdP = third;
-  this.fourthP = fourth;
-  this.fifthP = fifth;
-  this.sizeP = size;
+  this.first = first;
+  this.second = second;
+  this.third = third;
+  this.fourth = fourth;
+  this.fifth = fifth;
+  this.size = size;
   this.price = 4;
 
   Pizza.prototype.CalSizePrice = function () {
@@ -61,10 +61,6 @@ function Pizza(first, second, third, fourth, fifth, size) {
     }
   };
 
-  // Pizza.prototype.CalPrice = function () {
-  //   document.getElementById("#price-pizza").innerHTML = this.price;
-  // }
-
 };
 
 
@@ -76,22 +72,23 @@ function Pizza(first, second, third, fourth, fifth, size) {
 
 window.addEventListener("load", function () {
 
-  document.querySelector("form#pick").addEventListener("submit", function handlePick(event) {
+  document.querySelector("form#pick").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let pick1 = document.getElementById("firstTopping");
-    let pick11 = pick1.options[pick1.selectedIndex].value;
-    let pick2 = document.getElementById("secondTopping");
-    let pick22 = pick2.options[pick2.selectedIndex].value;
-    let pick3 = document.getElementById("thirdTopping");
-    let pick33 = pick3.options[pick3.selectedIndex].value;
-    let pick4 = document.getElementById("fourthTopping");
-    let pick44 = pick4.options[pick4.selectedIndex].value;
-    let pick5 = document.getElementById("fifthTopping");
-    let pick55 = pick5.options[pick5.selectedIndex].value;
+    const pick1 = document.getElementById("firstTopping");
+    const pick11 = pick1.options[pick1.selectedIndex].value;
+    const pick2 = document.getElementById("secondTopping");
+    const pick22 = pick2.options[pick2.selectedIndex].value;
+    const pick3 = document.getElementById("thirdTopping");
+    const pick33 = pick3.options[pick3.selectedIndex].value;
+    const pick4 = document.getElementById("fourthTopping");
+    const pick44 = pick4.options[pick4.selectedIndex].value;
+    const pick5 = document.getElementById("fifthTopping");
+    const pick55 = pick5.options[pick5.selectedIndex].value;
 
-    let sizePick = document.getElementById("pickSize");
-    let size = sizePick.options[sizePick.selectedIndex].value;
+    const sizePick = document.getElementById("pickSize");
+    const size = sizePick.options[sizePick.selectedIndex].value;
+    //console.log(size);
 
     //const priceShow = document.getElementById("price-pizza").text(this.price);
 
@@ -101,7 +98,6 @@ window.addEventListener("load", function () {
 
 
     PizzaOrder.CalSizePrice();
-    console.log(PizzaOrder);
     PizzaOrder.CalToppingsOne();
     PizzaOrder.CalToppingsTwo();
     PizzaOrder.CalToppingsThree();
@@ -111,7 +107,7 @@ window.addEventListener("load", function () {
 
 
 
-    document.getElementById("price-pizza").innerHTML = PizzaOrder.price;
+    document.getElementById("price-pizza").innerText = PizzaOrder.price;
     document.getElementById("topping-pizza").innerText = PizzaOrder.pick11 + "," + PizzaOrder.pick22 + "," + PizzaOrder.pick33 + "," + PizzaOrder.pick44 + "," + PizzaOrder.pick55;
     document.getElementById("size-pizza").innerText = PizzaOrder.size;
   })
